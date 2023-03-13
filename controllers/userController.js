b986a1d36d58c029927f6349b0184f3e5e29ea1d
osjarju <115750838+osjarju@users.noreply.github.com>
2023-03-13 14:01:51 -0700

    Error message

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,25 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
+
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .then((user) =>
+                !post
+                    ? res.status(404).json({ message: 'No user with that ID found' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    //Create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
+};
---
+++ utils/seeds.js
@@ -0,0 +1,39 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+const {
+    getRandonName,
+    getRandomIndex,
+    getRandomUser,
+    getRandomThoughts,
+} = require('./data');
+
+console.time('seeding');
+
+connection.once('open', async () => {
+
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    const thoughts = [...getRandomThoughts(10)];
+    const users = [];
+
+    const makeUser = (text) => {
+        users.push({
+            text,
+            username: getRandonName().split(' ')[0],
+            thoughts: [thoughts[getRandomIndex(thoughts)]._id],
+        });
+    };
+
+    await Thought.collection.inserMany(thoughts);
+
+    thoughts.forEach(() => makeUser(getRandomUser(10)));
+
+    await User.collection.inserMany(users);
+
+    console.table(thoughts);
+    console.table(users);
+    console.timeEnd('seeding complete');
+    process.exit(0);
+});
